851c42a4d5de1e2a2581f29f9379ab87abc2ebce
Nafis Fuad <fuadbd14@gmail.com>
2019-10-16 19:58:08 +0600

    Add campaign manager component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import Head from 'next/head'
 
+// Import Custom Components
+import CampaignManager from '../components/CampaignManager'
+
 const Home = () => (
   <>
     <Head>
       <title>Campaign Manager</title>
     </Head>
+    <CampaignManager />
   </>
 )
 
